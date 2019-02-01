e1cadb68d5520de7d392b5bb367ae594fbf46eee
Anthony-08 <ajsanchezolmo@gmail.com>
2019-02-01 10:09:39 +0100

    Modificado Servicio (/ofertas y Vigentes)

--- src/app/ofertas/ofertas.service.ts
+++ src/app/ofertas/ofertas.service.ts
@@ -8,15 +8,18 @@ export class OfertasService {
   constructor(private httpClient: HttpClient) { }
 
   public getOfertas() {
-    return this.httpClient.get<Ofertas[]>('http://localhost:3000/users');
+    return this.httpClient.get<Ofertas[]>('http://localhost:3000/ofertas');
+  }
+  public getOfertasVigentes(){
+    return this.httpClient.get<Ofertas[]>('http://localhost:3000/ofertas/Vigentes');
   }
   public getOfertasById(id: number){
-    return this.httpClient.get<Ofertas>('http://localhost:3000/users/' + id);
+    return this.httpClient.get<Ofertas>('http://localhost:3000/ofertas/' + id);
   }
   public postOferta(oferta: Ofertas){
-    return this.httpClient.post('http://localhost:3000/users', oferta);
+    return this.httpClient.post('http://localhost:3000/ofertas', oferta);
   }
   public deleteOferta(id: number){
-    return this.httpClient.delete('http://localhost:3000/users/' + id);
+    return this.httpClient.delete('http://localhost:3000/ofertas/' + id);
   }
 }
