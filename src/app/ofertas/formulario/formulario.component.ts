70d9c9dd7bd0bbde9a03c18e6f50417358a79c6d
Alejandro <alemr92@gmail.com>
2019-02-01 11:12:54 +0100

    formularioOfertas

--- src/app/ofertas/formulario/formulario.component.ts
+++ src/app/ofertas/formulario/formulario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Ofertas } from '../models/ofertas';
+import { OfertasService} from '../ofertas.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string;
+  ofertas: Ofertas;
+
+  constructor(private route: ActivatedRoute, private router: Router, private ofertasService: OfertasService) { 
+    this.ofertas = <Ofertas>{};
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(oferta: Ofertas){
+    oferta.id = null;
+    this.ofertasService.postOferta(oferta).subscribe(
+      new_oferta => {
+        this.router.navigate(['/ofertas', oferta.id]);
+      },
+      error => this.errorMessage = <any> error
+    );
+
   }
 
 }
