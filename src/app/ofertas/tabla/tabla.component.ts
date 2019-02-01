1c2f2c77aac2e9d518f3c9eea06d4d47aa326303
josedavidrs96 <josedavidrs96@gmail.com>
2019-02-01 13:47:57 +0100

    Tabla listado completada

--- src/app/ofertas/tabla/tabla.component.ts
+++ src/app/ofertas/tabla/tabla.component.ts
@@ -1,6 +1,7 @@
 import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
 import { Ofertas } from '../models/ofertas';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabla',
@@ -11,12 +12,22 @@ export class TablaComponent implements OnInit {
 
   public ofertaLista: Array<Ofertas>;
 
-  constructor(private ofertasService: OfertasService) { }
+  constructor(private ofertasService: OfertasService, private router: Router) { }
 
   ngOnInit() {
     this.ofertasService.getOfertas().subscribe( data => {
       this.ofertaLista = data;
-    })
+    });
+  }
+
+  public goFormulario(){
+    this.router.navigate(['ofertas/agregar']);
+  }
+
+  public borrarOferta(id: number){
+    this.ofertasService.deleteOferta(id).subscribe( data => {
+      this.router.navigate(['ofertas']);
+    });
   }
 
 }
