dc243f18ab5f75d9f01975177abc8a3b1932db47
Anthony-08 <ajsanchezolmo@gmail.com>
2019-02-01 12:21:41 +0100

    Cambios css y service

--- src/app/ofertas/ofertas.service.ts
+++ src/app/ofertas/ofertas.service.ts
@@ -5,26 +5,26 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable()
 export class OfertasService {
 
-  public ruta = 'http://localhost:9966/petclinic/api/';
+  public ruta = 'http://localhost:9966/petclinic/api/ofertas/';
 
   constructor(private httpClient: HttpClient) { }
 
   public getOfertas() {
-    return this.httpClient.get<Ofertas[]>(this.ruta + 'ofertas');
+    return this.httpClient.get<Ofertas[]>(this.ruta);
   }
   public getOfertasVigentes(){
     return this.httpClient.get<Ofertas[]>(this.ruta + 'listOfertasVigentes');
   }
   public getOfertasById(id: number){
-    return this.httpClient.get<Ofertas>(this.ruta + 'ofertas/' + id);
+    return this.httpClient.get<Ofertas>(this.ruta + id);
   }
   public postOferta(oferta: Ofertas){
     const headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
-    return this.httpClient.post(this.ruta + 'ofertas', oferta, { headers });
+    return this.httpClient.post(this.ruta, oferta, { headers });
   }
   public deleteOferta(id: number){
-    return this.httpClient.delete(this.ruta + 'ofertas/' + id);
+    return this.httpClient.delete(this.ruta + id);
   }
 }
